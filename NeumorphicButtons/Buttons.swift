f2983dd426a8056647e240455485391c7b17449d
Justin <tonybek.96@gmail.com>
2021-06-28 19:42:12 +0900

    added background color

--- NeumorphicButtons/Buttons.swift
+++ NeumorphicButtons/Buttons.swift
@@ -8,19 +8,49 @@
 import SwiftUI
 
 struct Buttons: View {
+    @State var tap = false
+    @State var press = false
+    
     var body: some View {
         VStack {
             Text("Button")
                 .font(.system(size:20, weight: .semibold, design: .rounded))
                 .frame(width:200, height: 60)
-                .background(Color.white)
+                .background(ZStack{
+                    Color(#colorLiteral(red: 0.8980392157, green: 0.9333333333, blue: 1, alpha: 1))
+                    RoundedRectangle(cornerRadius: 16, style: .continuous)
+                        .foregroundColor(.white)
+                        .blur(radius: 4)
+                        .offset(x: -8, y: -8)
+                    
+                    RoundedRectangle(cornerRadius: 16, style: .continuous)
+                        .fill(
+                            LinearGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 0.9019607843, green: 0.9294117647, blue: 0.9882352941, alpha: 1)), Color.white]), startPoint: .topLeading, endPoint: .bottomTrailing)
+                        )
+                        .padding(2)
+                        .blur(radius: 2)
+                })
                 .clipShape(RoundedRectangle(cornerRadius: 16, style: .continuous))
-                .shadow(color: Color(#colorLiteral(red: 0.7608050108, green: 0.8164883852, blue: 0.9259157777, alpha: 1)), radius: 20, x: 20, y: 20)
-                .shadow(color: Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)), radius: 20, x: -20, y: -20)
+                .shadow(color: press ? Color(#colorLiteral(red: 0.7450980392, green: 0.8, blue: 0.8980392157, alpha: 1)) : Color(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)), radius: 20, x: -20, y: -20)
+                .shadow(color: press ? Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)) : Color(#colorLiteral(red: 0.7294117647, green: 0.7843137255, blue: 0.8941176471, alpha: 1)), radius: 20, x: 20, y: 20)
+                .scaleEffect(tap ? 1.2 : 1 )
+                .gesture(
+                    LongPressGesture().onChanged{value in
+                        self.tap = true
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                            self.tap = false
+                        }
+                    }
+                    .onEnded{value in
+                        self.press.toggle()
+                        
+                    }
+                )
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(Color(#colorLiteral(red: 0.8980392157, green: 0.9333333333, blue: 1, alpha: 1)))
+        .background(Color(#colorLiteral(red: 0.7608050108, green: 0.8164883852, blue: 0.9259157777, alpha: 1)))
         .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+        .animation(.spring(response: 0.5, dampingFraction: 0.5, blendDuration: 0))
     }
 }
 
