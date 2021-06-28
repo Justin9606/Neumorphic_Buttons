38a617712e47ad109f59100fd64d2edc5f6bbe50
Justin <tonybek.96@gmail.com>
2021-06-28 18:49:11 +0900

    initial Comment

---
+++ NeumorphicButtons/NeumorphicButtonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeumorphicButtonsApp.swift
+//  NeumorphicButtons
+//
+//  Created by Justin on 2021/06/28.
+//
+
+import SwiftUI
+
+@main
+struct NeumorphicButtonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
